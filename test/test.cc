5fcd8ad326f85b8bdf69615d15d18bc8819cf179
Gareth Ferneyhough <gareth@space.vu>
2016-08-01 10:05:12 -0700

    simple gtest works

---
+++ test/test.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+class StateTest : public testing::Test {
+protected:
+
+    virtual void SetUp() {
+
+    }
+
+    virtual void TearDown() {
+
+    }
+};
+
+TEST_F(StateTest, createState) {
+    ASSERT_TRUE(true);
+}
