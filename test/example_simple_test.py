fcfea56bd159c00b7df74d1f36f25ba39008ef64
Gareth Ferneyhough <gareth@space.vu>
2016-08-01 22:20:18 -0700

    integration tests WIP

--- test/example_simple_test.py
+++ test/example_simple_test.py
@@ -1,6 +1,14 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
+import os
+from subprocess import call
 
-def test_answer():
-    assert func(3) == 5
+test_directory_name = "testdir"
+
+def setup():
+    if os.path.isdir(test_directory_name):
+        call(["fusermount", "-u", test_directory_name])
+        os.rmdir(test_directory_name)
+    os.mkdir(test_directory_name)
+    call(["../examples/bin/simple", test_directory_name])
+    
+def test_getattr():
+    assert test_directory_name in os.listdir('.')
